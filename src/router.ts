8ee2454eff5b8c02f69a1417955be3485127df1e
Jean-Luc CANELA <jlcanelam@gmail.com>
2026-05-20 22:30:19 +0200

    Fix url for deployment

--- src/router.ts
+++ src/router.ts
@@ -6,8 +6,14 @@ export type RouterContext = {
   runtime: AppRuntime;
 };
 
+const baseUrl = import.meta.env.BASE_URL;
+const routerBasepath = baseUrl.endsWith("/")
+  ? baseUrl.slice(0, -1) || "/"
+  : baseUrl;
+
 export const router = createRouter({
   routeTree,
+  basepath: routerBasepath,
   context: {
     runtime: undefined!,
   },
